9b2f7844b389b2bb1f91091f2c14e32fbd04ba9f
Zakarie Ortiz <ZOrtiz42@outlook.com>
2019-08-16 16:39:38 -0400

    ADD
    -Added Utility file for refactoring of the project.

---
+++ MyDestinyV0.1/MyDestiny/MyDestinyProject/MyDestiny/Utility/APIUtility.swift
@@ -0,0 +1,29 @@
+//
+//  APIUtility.swift
+//  MyDestiny
+//
+//  Created by Zakarie Ortiz on 8/16/19.
+//  Copyright © 2019 Zakarie Ortiz. All rights reserved.
+//
+
+import Foundation
+
+class APIUtility {
+    
+    //Variables have to be defined as static let, like the below example.
+    
+    static let API_ENDPOINT_BLANK: String = "hello there"
+    
+    //All of the functions that are being used to make api calls will be placed into here.
+    
+    //Will also need to place the contract variables in here.
+    
+    /**
+     Start with the AuthenticateVC and then move down the list.
+    */
+    
+    static func postRequest() {
+        
+    }
+    
+}
